9256a4e7dc6ceaf12aab176329136bf2473bd43a
Carlos Isaias Ruiz <zimoo354@gmail.com>
2020-03-04 23:08:12 -0600

    initial commit

---
+++ kueskifields.js
@@ -0,0 +1,202 @@
+// First Step
+first = [
+	{
+		name: "name",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "lastname_a",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "lastname_b",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "b_day",
+		id: "undefined",
+		type: "undefined"
+	},
+	{
+		name: "b_month",
+		id: "undefined",
+		type: "undefined"
+	},
+	{
+		name: "b_year",
+		id: "undefined",
+		type: "undefined"
+	},
+	{
+		name: "gender",
+		id: "gender_hombre",
+		type: "radio"
+	},
+	{
+		name: "gender",
+		id: "gender_mujer",
+		type: "radio"
+	},
+	{
+		name: "bornplace",
+		id: "undefined",
+		type: "undefined"
+	},
+	{
+		name: "curp",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "mobile",
+		id: "mobile",
+		type: "tel"
+	},
+	{
+		name: "accept_check_phone",
+		id: "accept_check_phone",
+		type: "checkbox"
+	},
+	{
+		name: "home_phone",
+		id: "home_phone",
+		type: "tel"
+	}
+];
+
+
+// Second Step
+second = [
+	{
+		name: "street",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "ext_number",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "inner_number",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "postal_code",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "state",
+		id: "undefined",
+		type: "undefined"
+	},
+	{
+		name: "city",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "neighborhood",
+		id: "undefined",
+		type: "text"
+	},
+];
+
+// Third Step
+third = [
+	{
+		name: "salary",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "is_salary_bank_deposit",
+		id: "is_salary_bank_deposit_yes",
+		type: "radio"
+	},
+	{
+		name: "is_salary_bank_deposit",
+		id: "is_salary_bank_deposit_no",
+		type: "radio"
+	},
+	{
+		name: "family_salary",
+		id: "undefined",
+		type: "text"
+	},
+	{
+		name: "undefined",
+		id: "circulo_validation_flag",
+		type: "hidden"
+	},
+	{
+		name: "has_credit_card",
+		id: "credit_card_yes",
+		type: "radio"
+	},
+	{
+		name: "has_credit_card",
+		id: "credit_card_no",
+		type: "radio"
+	},
+	{
+		name: "credit_card_digits",
+		id: "credit_card_digits",
+		type: "text"
+	},
+	{
+		name: "has_house_loan",
+		id: "house_loan_yes",
+		type: "radio"
+	},
+	{
+		name: "has_house_loan",
+		id: "house_loan_no",
+		type: "radio"
+	},
+	{
+		name: "has_auto_loan",
+		id: "auto_loan_yes",
+		type: "radio"
+	},
+	{
+		name: "has_auto_loan",
+		id: "auto_loan_no",
+		type: "radio"
+	},
+	{
+		name: "credit_history_selfperception",
+		id: "selfperception_2",
+		type: "undefined"
+	},
+	{
+		name: "circulo_authorize",
+		id: "circulo_authorize",
+		type: "checkbox"
+	},
+	{
+		name: "loan_typedesc",
+		id: "loan_typedesc",
+		type: "hidden"
+	},
+	{
+		name: "undefined",
+		id: "loan_typedesc_category",
+		type: "undefined"
+	},
+	{
+		name: "loan_typedesc_subcategory",
+		id: "loan_typedesc_subcategory",
+		type: "undefined"
+	},
+	{
+		name: "applying_by_self",
+		id: "applying_by_self",
+		type: "checkbox"
+	},
+];
